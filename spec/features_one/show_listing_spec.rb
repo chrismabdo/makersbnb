b02c81e70494e5224cdfe6a5384dddee433fd674
Kiki Dawson <kikiadawson@gmail.com>
2021-01-05 14:11:48 +0000

    coverage changes

--- spec/features_one/show_listing_spec.rb
+++ spec/features_one/show_listing_spec.rb
@@ -1,7 +1,7 @@
 feature 'view list of all spaces' do
     scenario 'can see all the spaces avaliable' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO users (username, user_email, user_password) VALUES ('Kiki', 'kiki@massivecastle.com', 'castle castle');")
+    # connection.exec("INSERT INTO users (username, user_email, user_password) VALUES ('Kiki', 'kiki@massivecastle.com', 'castle castle');")
     connection.exec("INSERT INTO spaces (name, description, price) VALUES ('Cave', 'large cave in Devon', '£5.00');")
     visit '/listings'
 
