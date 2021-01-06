ffc1f022a3c764350c8228c140c83116ba2eb64b
Kiki Dawson <kikiadawson@gmail.com>
2021-01-06 12:38:31 +0000

    db create requests table updated

--- db/03_creating_requests_table.sql
+++ db/03_creating_requests_table.sql
@@ -1,1 +1,1 @@
-CREATE TABLE requests (request_id SERIAL PRIMARY KEY, space_id INTEGER REFERENCES spaces(space_id), owner_id INTEGER REFERENCES users(user_id), guest_id INTEGER REFERENCES users(user_id), dates VARCHAR(60));
+CREATE TABLE requests (request_id SERIAL PRIMARY KEY, space_id INTEGER REFERENCES spaces(space_id), guest_id INTEGER REFERENCES users(user_id), dates VARCHAR(60));
